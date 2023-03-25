153e58ca2c18a2023227c472e465012191dbecf0
Ali Zafar <frogbuttdev@gmail.com>
2023-03-25 15:10:46 +0000

    updated django settings for python anywhere and readded media dir

--- shelves_project/settings.py
+++ shelves_project/settings.py
@@ -23,12 +23,17 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's($w!h)a3h7xrp($b#hv4$)0-iy9$8jd&r^m8wd!!ndv%p=-m*'
+key = None
+
+with open('secret.key') as f:
+    key = f.read().strip()
+
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['alizafar155.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['alizafar155.pythonanywhere.com']
 
 
 
